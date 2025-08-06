25687529cb7803acf5c8cceec4caa4c95fa6fe97
sangjun park <guaba0412@gmail.com>
2025-08-06 16:15:13 +0900

    git push first commit (go lang)

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 사용자 정보를 담을 구조체 정의
+type User struct {
+	ID    int    `json:"id"`    // 사용자 ID
+	Name  string `json:"name"`  // 이름
+	Email string `json:"email"` // 이메일
+}
+
+// 사용자 정보를 담을 구조체 정의
+type FepData struct {
+	Port             int    `json:"port"`           // 포트번호
+	ReceivedTime     string `json:"time"`           // 시간
+	TotalCount       int    `json:"total_count"`    // 총 합계
+	ErrorCount       int    `json:"error_count"`    // 오류
+	CurrentCount     int    `json: "current_count"` // 현재 수신값
+	ConnectionStatus string `json: "status"`        // 연결상태
+}
+
+// 사용자 정보를 메모리에 저장할 슬라이스
+var users []User
+var nextID int = 1 // 사용자 ID 자동 증가 용도
+
+var (
+	fepMap = make(map[int]FepData) // 포트번호 -> 최신 데이터
+	mutex  = sync.RWMutex{}
+)
+
+func main() {
+	// 기본 라우터 생성
+	router := gin.Default()
+
+	// 사용자 목록 조회 API
+	router.GET("/users", getUsers)
+
+	// 사용자 등록 API
+	router.POST("/users", createUser)
+
+	// 8080 포트에서 서버 실행
+	router.Run(":8080")
+}
+
+// 사용자 목록 조회 핸들러
+func getUsers(c *gin.Context) {
+	c.JSON(http.StatusOK, users) // 모든 사용자 정보를 JSON으로 반환
+}
+
+// 사용자 등록 핸들러
+func createUser(c *gin.Context) {
+	var newUser User
+
+	// 요청 body에서 JSON 데이터를 파싱하여 newUser에 바인딩
+	if err := c.BindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "잘못된 요청입니다"})
+		return
+	}
+
+	// ID 자동 할당
+	newUser.ID = nextID
+	nextID++
+
+	// 메모리에 사용자 추가
+	users = append(users, newUser)
+
+	// 등록된 사용자 정보를 반환
+	c.JSON(http.StatusCreated, newUser)
+}
---
+++ test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+    CURL *curl;
+    CURLcode res;
+
+
+    const char *json_data = "{\"id\":1,\"name\":\"상준222\",\"email\":\"sangjun@example.com\"}";
+
+    struct curl_slist *headers = NULL;
+    
+    curl = curl_easy_init();
+    if (curl) {
+        /* 헤더 설정 */
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        headers = curl_slist_append(headers, "Accept: application/json");
+
+        /* 요청할 URL 설정 */
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/users");
+
+        /* 강제 IPv4로 전송*/
+        curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+
+        /* POST 방식으로 설정 */
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+
+        /* JSON 데이터 전송 */
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data);
+
+        /* 헤더 적용 */
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        /* 응답을 stdout에 출력 */
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout);
+
+        /* 요청 수행 */
+        res = curl_easy_perform(curl);
+
+        /* 결과 확인 */
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() 실패: %s\n", curl_easy_strerror(res));
+
+        /* 정리 */
+        curl_slist_free_all(headers);
+        curl_easy_cleanup(curl);
+    }
+
+    return 0;
+}
